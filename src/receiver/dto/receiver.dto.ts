732fbcf16d4ac6ad852b33e32d22af50687ba1a4
jaeuk.lee <dlwodnrsms@naver.com>
2024-04-05 17:46:54 +0900

    added sync dto

--- src/receiver/dto/receiver.dto.ts
+++ src/receiver/dto/receiver.dto.ts
@@ -12,9 +12,12 @@ export class ReceiverDto{
   /**
    * port가 존재할 경우 입력합니다. 기본값은 6500 입니다. 
    */
+
+
+  
   @IsOptional()
-  @IsNumber()
-  readonly checkIP:number;
+  @IsString()
+  readonly check_ip:string;
 
 
 }
