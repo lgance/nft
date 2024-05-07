73ae481f486f7b8f453f13346268510845a62dc6
jaeuk.lee <dlwodnrsms@naver.com>
2024-05-07 16:33:12 +0900

    receiver dto fixed

--- src/receiver/dto/receiver.dto.ts
+++ src/receiver/dto/receiver.dto.ts
@@ -7,17 +7,30 @@ export class ReceiverDto{
   // @IsNumber()
   // readonly srcIP:number;
 
-
-
   /**
    * port가 존재할 경우 입력합니다. 기본값은 6500 입니다. 
    */
-
-
   
   @IsOptional()
   @IsString()
   readonly check_ip:string;
+
+
+  @IsOptional()
+  @IsString()
+  readonly is_ncp_services:string;
+
+
+
+  @IsOptional()
+  @IsString()
+  readonly protocol:string;
+
+
+  @IsOptional()
+  readonly is_check_tcp_state:boolean;
+ 
+
 
 
 }
